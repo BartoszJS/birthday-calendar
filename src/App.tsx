683aad00798b1d05f381a1a620b7b48f3d05a9c2
BartoszJS <bartosz290300@wp.pl>
2023-04-14 23:52:29 +0200

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Calendar from "./components/Calendar/Calendar";
 import Home from "./components/Home/Home";
-import List from "./components/Home/List";
 import BirthdayForm from "./components/BirthdayForm/BirthdayForm";
 
 function App() {
-  const items = ["Item 1", "Item 2", "Item 3"];
   return (
     <div className='App'>
       <BrowserRouter>
